d0ba4a7cf99f0b3dc39f9a86647f775ea4005175
Thomas <thomas.lisankie@gmail.com>
2020-04-24 10:15:59 -0400

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to tm-code-assessment
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/tm_code_assessment/core_test.clj
@@ -0,0 +1,7 @@
+(ns tm-code-assessment.core-test
+  (:require [clojure.test :refer :all]
+            [tm-code-assessment.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
